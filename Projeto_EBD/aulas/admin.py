8141c4d218505f9afb43b9bae50009c9288eac70
Isaias <60967597+Isaias79@users.noreply.github.com>
2025-03-09 16:15:31 +0000

    Models

--- Projeto_EBD/aulas/admin.py
+++ Projeto_EBD/aulas/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Aula
+from .models import *
 
+admin.site.register(Igreja)
+admin.site.register(Usuario)
+admin.site.register(Aluno)
 admin.site.register(Aula)
+admin.site.register(Classe)
+admin.site.register(Diario)
+admin.site.register(Presenca)
+admin.site.register(Professor)
+admin.site.register(Trimestre)
+admin.site.register(Matricula)
 
 # Register your models here.
