454857f52c8bad083ed1206641c6a2e7e1f474be
Isaias <60967597+Isaias79@users.noreply.github.com>
2025-03-09 16:13:07 +0000

    Modelos

--- Projeto_EBD/aulas/models.py
+++ Projeto_EBD/aulas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Igreja(models.Model):
     id = models.AutoField(primary_key = True)
@@ -17,12 +18,18 @@ class Usuario(models.Model):
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=50) # update: "informe seu cargo"
 
+    def __str__(self):
+        return self.nome
+
 
 class Classe(models.Model):
     id = models.AutoField(primary_key=True)
     nome_igreja = models.ForeignKey(Igreja, on_delete=models.CASCADE) 
     nome = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nome
+    
 class Trimestre(models.Model):
     id = models.AutoField(primary_key=True)
     nome_igreja = models.ForeignKey(Igreja, on_delete=models.CASCADE) # SE UM DELETA OS OUTROS TMB
@@ -31,20 +38,29 @@ class Trimestre(models.Model):
     ano = models.CharField(max_length=50) # PODERIA SER INT?
     concluido = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.nome
+
 class Aula(models.Model):
     id = models.AutoField(primary_key=True)
-    trimestre_da_aula = models.ForeignKey(Trimestre, on_delete=models.CASCADE)
+    trimestre_da_aula = models.ForeignKey(Trimestre, on_delete=models.CASCADE, null=True)
 
 
     aula = models.CharField(max_length=200)
-    data_prevista = models.DateField() # VALOR PADRÃO DA DATA DE HOJE
+    data_prevista = models.DateField(default=timezone.now) # VALOR PADRÃO DA DATA DE HOJE
     concluida = models.BooleanField(default=True)
     # data = models.DateTimeField() # VALOR PADRÃO DA DATA DE HOJE
+
+    def __str__(self):
+        return self.nome
 
 class Professor(models.Model):
     id = models.AutoField(primary_key=True)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE) ## ANTES DE CRIAR O PROFESSOR TEM DE CRIAR O USUÁRIO
     classe = models.ForeignKey(Classe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
 
 class Diario(models.Model):
     id = models.AutoField(primary_key=True)
@@ -60,12 +76,18 @@ class Diario(models.Model):
     ofertas = models.BooleanField(default=True)
     dizimos = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.nome
+
 class Aluno(models.Model):
     id = models.AutoField(primary_key=True)
     nome_igreja = models.ForeignKey(Igreja, on_delete=models.CASCADE)
 
     nome = models.CharField(max_length=50)
     data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
 
 class Presenca(models.Model):
     id = models.AutoField(primary_key=True)
@@ -74,8 +96,14 @@ class Presenca(models.Model):
 
     presenca = models.CharField(max_length=8) #PRESENTE / AUSENTE
 
+    def __str__(self):
+        return self.nome
+
 class Matricula(models.Model):
     id = models.AutoField(primary_key=True)
     selecione_aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
     trimestre = models.ForeignKey(Trimestre, on_delete=models.CASCADE)
     classe = models.ForeignKey(Classe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
