87e5328a397405cc0a19c7bda97b0be1c7059008
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-08-31 19:34:13 -0300

    feat: remove veutify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
-
-import 'vuetify/styles'
 
 import App from './App.vue'
 import router from './router'
@@ -10,7 +7,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(createVuetify())
 app.use(router)
 
 app.mount('#app')
