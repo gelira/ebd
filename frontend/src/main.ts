3efe346dba8a56a77596d1eed3e195eb140b24cd
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-08-31 19:54:19 -0300

    Revert "feat: remove veutify"
    This reverts commit 87e5328a397405cc0a19c7bda97b0be1c7059008.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+
+import 'vuetify/styles'
 
 import App from './App.vue'
 import router from './router'
@@ -7,6 +10,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createVuetify())
 app.use(router)
 
 app.mount('#app')
