dcd7ffe8b2632fc7ff06b30c2b0cc09504417785
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-09-10 20:24:09 -0300

    feat: remove main app

--- backend/main/authentication.py
+++
@@ -1,8 +0,0 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-from .models import Usuario
-
-class CustomJWTAuthentication(JWTAuthentication):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.user_model = Usuario
