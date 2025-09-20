4f62b632c88de4286435f1e78e7d0ff3d7f52ba2
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-09-19 21:42:03 -0300

    feat: improve home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import AppBar from '@/components/AppBar.vue'
+import { useNavigationStore } from '@/stores/navigation'
 import { useUsuarioStore } from '@/stores/usuario'
 import { onMounted } from 'vue'
 
 const usuarioStore = useUsuarioStore()
+const navigationStore = useNavigationStore()
 
 onMounted(() => {
   usuarioStore.fetchUserInfo()
+    .catch(() => {
+      navigationStore.goToLogin()
+    })
 })
 </script>
 
 <template>
   <AppBar />
   <v-container class="mt-16">
-    <h1>Home</h1>
+    <h1>Home {{ usuarioStore.nome }}</h1>
   </v-container>
 </template>
