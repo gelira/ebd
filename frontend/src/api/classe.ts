98c50aaef638729db64e2afd4d31091cfd649177
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-09-19 22:49:09 -0300

    feat: classe store

--- frontend/src/api/classe.ts
+++ frontend/src/api/classe.ts
@@ -1,13 +1,5 @@
 import { authApiClient } from './client'
 
-export function apiGetClasses(congregacaoId: string) {
-  return authApiClient().get<{ classes: Classe[] }>(`/api/congregacoes/${congregacaoId}/classes`)
-}
-
-export function apiGetClasseMaticulas(classeId: string, periodoId: string) {
-  return authApiClient().get<{ alunos: Aluno[] }>(`/api/classes/${classeId}/matriculas`, {
-    params: {
-      periodo_uid: periodoId
-    }
-  })
+export function apiGetClasses() {
+  return authApiClient().get<{ classes: Classe[] }>('/api/classes')
 }
--- frontend/src/stores/classe.ts
+++ frontend/src/stores/classe.ts
@@ -1,55 +1,27 @@
-import { computed, reactive } from 'vue'
+import { apiGetClasses } from '@/api/classe'
 import { defineStore } from 'pinia'
-import { apiGetClasses, apiGetClasseMaticulas } from '@/api/classe'
+import { computed, reactive } from 'vue'
 
 interface State {
-  congregacaoId: string
-  classeId: string
-  periodoId: string
   classes: Classe[]
-  alunosClasse: Aluno[]
 }
 
 export const useClasseStore = defineStore('classe', () => {
   const state = reactive<State>({
-    congregacaoId: '',
-    classeId: '',
-    periodoId: '',
-    classes: [],
-    alunosClasse: []
+    classes: []
   })
 
   const classes = computed(() => state.classes)
-  const alunosClasse = computed(() => state.alunosClasse)
-  const congregacaoId = computed(() => state.congregacaoId)
-  const classeId = computed(() => state.classeId)
-  const periodoId = computed(() => state.periodoId)
 
-  function fetchClasses(congregacaoId: string) {
-    apiGetClasses(congregacaoId)
+  function fetchClasses() {
+    apiGetClasses()
       .then((response) => {
         state.classes = response.data.classes
-        state.congregacaoId = congregacaoId
       })
       .catch(() => {
         state.classes = []
-        state.congregacaoId = ''
       })
   }
 
-  function fetchClasseMaticulas(classeId: string, periodoId: string) {
-    apiGetClasseMaticulas(classeId, periodoId)
-      .then((response) => {
-        state.alunosClasse = response.data.alunos
-        state.classeId = classeId
-        state.periodoId = periodoId
-      })
-      .catch(() => {
-        state.alunosClasse = []
-        state.classeId = ''
-        state.periodoId = ''
-      })
-  }
-
-  return { classes, congregacaoId, classeId, periodoId, alunosClasse, fetchClasseMaticulas, fetchClasses }
+  return { classes, fetchClasses }
 })
