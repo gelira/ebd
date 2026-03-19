9bfc4974509579501ec46dbf0e12aa76bd69751c
Geraldo Eloi <geraldo.eloi11@gmail.com>
2026-03-18 22:39:26 -0300

    feat: verify token

--- app/_lib/utils/token.ts
+++ app/_lib/utils/token.ts
@@ -1,7 +1,9 @@
-import { sign } from 'jsonwebtoken'
+import { sign, verify } from 'jsonwebtoken'
 
 export const JWT_EXPIRES_IN = 60 * 60 * 8
 export const AUTH_TOKEN_COOKIE_NAME = 'auth_token'
+
+const JWT_ALGORITHM = 'HS512'
 
 export function generateToken(payload: { userId: number, userRole: string }) {
   const secretKey = process.env.JWT_SECRET_KEY
@@ -12,6 +14,18 @@ export function generateToken(payload: { userId: number, userRole: string }) {
 
   return sign(payload, secretKey, {
     expiresIn: JWT_EXPIRES_IN,
-    algorithm: 'HS512',
+    algorithm: JWT_ALGORITHM,
   })
+}
+
+export function verifyToken(token: string) {
+  const secretKey = process.env.JWT_SECRET_KEY
+
+  if (!secretKey) {
+    throw new Error('JWT_SECRET_KEY is not defined')
+  }
+
+  const decoded = verify(token, secretKey)
+
+  return decoded as { userId: number, userRole: string }
 }
