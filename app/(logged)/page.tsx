ce6d7f7430390076f78bb100cdd0257ab03f96e6
Geraldo Eloi <geraldo.eloi11@gmail.com>
2026-03-24 16:55:23 -0300

    feat: automatic redirect case the user has only one congregation

--- app/(logged)/page.tsx
+++ app/(logged)/page.tsx
@@ -1,11 +1,38 @@
+import { getCongregationsByUserId } from '@/app/_lib/db/congregation'
 import { getCurrentUser } from '@/app/_lib/services/auth'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default async function Page() {
   const user = await getCurrentUser()
 
+  if (!user) {
+    return null
+  }
+
+  const congregations = await getCongregationsByUserId(user.id)
+
+  if (congregations.length === 1) {
+    redirect(`/congregation/${congregations[0].id}`)
+  }
+
   return (
     <div>
-      <h1>{user?.role}</h1>
+      {congregations.length === 0 ? (
+        <h1>Você não está vinculado a nenhuma congregação</h1>
+      ) : (
+        <>
+          <h1>Escolha uma congregação</h1>
+          {congregations.map((congregation) => (
+            <Link
+              href={`/congregation/${congregation.id}`}
+              key={congregation.id}
+            >
+              {congregation.name}
+            </Link>
+          ))}
+        </>
+      )}
     </div>
   )
 }
