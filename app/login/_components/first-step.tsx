37d5f141d9d93b75f106a8b9779fbc8eb350938e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2026-03-28 23:49:18 -0300

    feat: styling login steps

--- app/login/_components/first-step.tsx
+++ app/login/_components/first-step.tsx
@@ -31,23 +31,37 @@ export default function FirstStep({ nextStep }: { nextStep: (authCodeId: number)
   }
 
   return (
-    <div>
-      <label>
-        Insira seu email:
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={
-            (e) => {
-              setEmail(e.target.value)
-              setErrorMessage('')
+    <>
+      <div>
+        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+          Email:
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            type="email"
+            autoComplete="email"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            onChange={
+              (e) => {
+                setEmail(e.target.value)
+                setErrorMessage('')
+              }
             }
-          }
-        />
-      </label>
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          onClick={handleSubmit}
+          disabled={pending}
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Enviar código
+        </button>
+      </div>
       {errorMessage && <p>{errorMessage}</p>}
-      <button onClick={handleSubmit} disabled={pending}>Próximo</button>
-    </div>
+    </>
   )
 }
--- app/login/_components/second-step.tsx
+++ app/login/_components/second-step.tsx
@@ -29,23 +29,37 @@ export default function SecondStep({ authCodeId }: { authCodeId: number }) {
   }
 
   return (
-    <div>
-      <label>
-        Insira o código enviado para seu email:
-        <input
-          type="text"
-          placeholder="Código"
-          name="code"
-          onChange={
-            (e) => {
-              setCode(e.target.value)
-              setErrorMessage('')
+    <>
+      <div>
+        <label htmlFor="code" className="block text-sm/6 font-medium text-gray-900">
+          Código enviado para o email:
+        </label>
+        <div className="mt-2">
+          <input
+            id="code"
+            type="text"
+            autoComplete="off"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            onChange={
+              (e) => {
+                setCode(e.target.value)
+                setErrorMessage('')
+              }
             }
-          }
-        />
-      </label>
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          onClick={handleSubmit}
+          disabled={pending}
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Entrar
+        </button>
+      </div>
       {errorMessage && <p>{errorMessage}</p>}
-      <button onClick={handleSubmit} disabled={pending}>Entrar</button>
-    </div>
+    </>
   )
 }
